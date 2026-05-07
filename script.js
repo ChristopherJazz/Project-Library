8eea43065fa3e0c9777074514b110c53fd0ad5e3
Christopher Jazz Esguerra <christopher.esguerra2@gmail.com>
2026-05-07 10:56:18 +0800

    Fixed modal and added Delete button with confirmation

--- script.js
+++ script.js
@@ -57,12 +57,86 @@ bookForm.addEventListener('submit', (event) => {
   // Store in array
   myLibrary.push(newBook);
 
+  // Render books
+  renderBooks();
    // Close modal
   modal.classList.remove('active');
 
   // Reset
   bookForm.reset();
 
-  // Test
-  console.log(myLibrary);
 });
+
+const bookList = document.querySelector('.book-list');
+
+// Render function
+function renderBooks() {
+  bookList.innerHTML = '';
+
+  myLibrary.forEach((book) => {
+    const card = document.createElement('div');
+    card.classList.add('book-card');
+
+    const title = document.createElement('h4');
+    title.textContent = book.title;
+
+    const author = document.createElement('p');
+    author.textContent = `Author: ${book.author}`;
+
+    const pages = document.createElement('p');
+    pages.textContent = `Pages: ${book.pages}`;
+
+    const read = document.createElement('p');
+    read.textContent = book.read ? 'Read' : 'Not Read';
+
+    // DELETE BUTTON
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add('delete-btn');
+
+    // store id in dataset
+    deleteBtn.dataset.id = book.id;
+
+    // delete action
+    deleteBtn.addEventListener('click', () => {
+    bookToDeleteId = book.id;
+
+    deleteMessage.textContent = `Delete "${book.title}"?`;
+
+    deleteModal.classList.add('active');
+});
+
+    confirmDeleteBtn.addEventListener('click', () => {
+      deleteBook(bookToDeleteId);
+      deleteModal.classList.remove('active');
+});
+
+    cancelDeleteBtn.addEventListener('click', () => {
+      deleteModal.classList.remove('active');
+      bookToDeleteId = null;
+});
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+    card.appendChild(deleteBtn);
+
+    bookList.appendChild(card);
+  });
+}
+
+function deleteBook(id) {
+  const index = myLibrary.findIndex(book => book.id === id);
+
+  if (index !== -1) {
+    myLibrary.splice(index, 1);
+    renderBooks();
+  }
+}
+
+const deleteModal = document.querySelector('.delete-modal-overlay');
+const deleteMessage = document.querySelector('.delete-message');
+const confirmDeleteBtn = document.querySelector('.confirm-delete');
+const cancelDeleteBtn = document.querySelector('.cancel-delete');
+
+let bookToDeleteId = null;
